650efbb709202190a60bf70597531f7dbaa7e956
Alina Kim <alinakim707@gmail.com>
2025-01-28 11:40:48 +0500

    Python Tuple

--- Tuples.py
+++ Tuples.py
@@ -146,3 +146,10 @@ fruits = ("apple", "banana", "cherry")
 mytuple = fruits * 2
 
 print(mytuple)
+
+"""
+Tuple Methods
+"""
+
+# count()	Returns the number of times a specified value occurs in a tuple
+# index()	Searches the tuple for a specified value and returns the position of where it was found
