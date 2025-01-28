a24e06c62dbc11fb133e5916a408435db2f5f93c
Alina Kim <alinakim707@gmail.com>
2025-01-28 11:54:25 +0500

    Python Dictionaries

--- Dict.py
+++ Dict.py
@@ -91,3 +91,82 @@ print(thisdict)
 
 thisdict.clear()
 print(thisdict)
+
+"""
+Loop Dictionaries
+"""
+for x in thisdict:
+  print(x)
+
+for x in thisdict:
+  print(thisdict[x])
+
+# You can also use the values() method to return values of a dictionary:
+for x in thisdict.values():
+  print(x)
+
+# You can use the keys() method to return the keys of a dictionary:
+for x in thisdict.keys():
+  print(x)
+
+# Loop through both keys and values, by using the items() method:
+for x, y in thisdict.items():
+  print(x, y)
+
+"""
+Copy Dictionaries
+"""
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+mydict = dict(thisdict)
+print(mydict)
+
+"""
+Nestes Dict
+"""
+# Create a dictionary that contain three dictionaries:
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+# Loop through the keys and values of all nested dictionaries:
+for x, obj in myfamily.items():
+  print(x)
+
+  for y in obj:
+    print(y + ':', obj[y])
+
+"""
+Dictionary Methods
+"""
+
+# clear()	Removes all the elements from the dictionary
+# copy()	Returns a copy of the dictionary
+# fromkeys()	Returns a dictionary with the specified keys and value
+# get()	Returns the value of the specified key
+# items()	Returns a list containing a tuple for each key value pair
+# keys()	Returns a list containing the dictionary's keys
+# pop()	Removes the element with the specified key
+# popitem()	Removes the last inserted key-value pair
+# setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+# update()	Updates the dictionary with the specified key-value pairs
+# values()	Returns a list of all the values in the dictionary
+
